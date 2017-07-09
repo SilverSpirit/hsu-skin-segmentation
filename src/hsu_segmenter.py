245d582a809abd8dae4c7acb99ffd2c145a2508a
Paridhi Srivastava <srivastavaparidhi15@gmail.com>
2017-07-08 21:19:52 -0400

    B chroma center method added

--- src/hsu_segmenter.py
+++ src/hsu_segmenter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import cv2
-import consts as ct
+import consts as *
 
 class HsuSegmenter:
     def __init__(self):
@@ -8,4 +8,13 @@ class HsuSegmenter:
     
     def get_mask(self, img):
         return img # this will return the mask, currently returns the image
-    
+
+
+    def get_center_b_chroma(self, Y):
+        center = 108
+        if Y < kl:
+            center += ((kl - Y) * (118-108)) / (kl - ymin)
+        elif kh < Y:
+            center += ((Y - kh) * (118 - 108)) / (ymax - kh)
+
+        return center
